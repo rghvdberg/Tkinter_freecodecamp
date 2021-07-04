5f754c0d813db361ac072d89ea16c330a089fd02
Rob van den Berg <rghvdberg@gmail.com>
2021-07-04 13:28:40 +0200

    Radio buttons with loop and Order button

--- radio_buttons.py
+++ radio_buttons.py
@@ -6,23 +6,34 @@ from PIL import ImageTk, Image
 root = Tk()
 root.title("Radio Buttons")
 
-r = IntVar()
-r.set(1)
+TOPPINGS = [
+    ("Pepperoni", "Pepperoni"),
+    ("Cheese", "Cheese"),
+    ("Mushroom", "Mushroom"),
+    ("Onion", "Onion"),
+]
+
+pizza = StringVar()
+# if there's no default value set than all radio buttons are greyed out
+pizza.set("Pepperoni")
+
 # my_label has to be defined before 'clicked' function otherwise my_label.configure() doesn't work
-my_label = Label(root, text=r.get())
+my_label = Label(root, text=pizza.get())
+
+for txt, topping in TOPPINGS:
+    Radiobutton(root, text=txt, variable=pizza,
+                value=topping).pack(anchor=W, pady=5)
+
+bt_order = Button(root, text="Place Order",
+                  command=lambda: clicked(pizza.get())).pack(padx=20)
+
 
 def clicked(value):
     global my_label
-    print(value)
-    my_label.configure(text=str(value))
+    my_label.configure(text=value)
 
 
-Radiobutton(root, text="Option 1", variable=r, value=1,
-            command=lambda: clicked(r.get())).pack()
-Radiobutton(root, text="Option 2", variable=r, value=2,
-            command=lambda: clicked(r.get())).pack()
-
-# my_label = Label(root, text=r.get()).pack()
 my_label.pack()
 
+# it looks like one can just call mainloop() instead of root.mainloop()?
 mainloop()
